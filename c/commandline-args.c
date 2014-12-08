facd5db4a591e5d4405d6dacc1aa96d3a9c25ae6
M <michi@imac.lan>
2014-12-08 18:28:03 +0100

    Minor

--- c/commandline-args.c
+++ c/commandline-args.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
-	printf("Program name (prog_name): %s\n", argv[0]);
-	printf("Number of arguments (num_args): %d\n", argc-1);
-	printf("First argument (first_arg): %s\n", argv[1]);
+	char *prog_name = argv[0];
+	int num_args = argc - 1;
+	char *first_arg = argv[1];
+
+	printf("Program name (prog_name): %s\n", prog_name);
+	printf("Number of arguments (num_args): %d\n", num_args);
+	printf("First argument (first_arg): %s\n", first_arg);
 	exit(0);
 }
