9d32d5e13b97ad63c92868a7e37773e97adc0b96
Michael <michael.mustun@gmail.com>
2014-12-08 17:43:27 +0100

    minor

--- perl/commandline-args.pl
+++ perl/commandline-args.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 $numArgs = $#ARGV + 1;
-print "Number of command line arguments: $numArgs\n";
+print "Number of arguments: $numArgs\n";
 
 foreach $argnum (0 .. $#ARGV) {
 	print "$ARGV[$argnum]\n";
