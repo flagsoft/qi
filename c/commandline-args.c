28adbc1f3a19b3ebba168830c90289103eadc3d8
Michael <michael.mustun@gmail.com>
2014-12-08 19:37:39 +0100

    minor

--- c/commandline-args.c
+++ c/commandline-args.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+
 int main(int argc, char *argv[])
 {
 	printf("Program name (prog_name): %s\n", argv[0]);
 	printf("Number of arguments (num_args): %d\n", argc-1);
 	printf("First argument (first_arg): %s\n", argv[1]);
-	exit(0);
+	
+	if (argc > 1) {
+		for (count = 1; count < argc; count++) {
+			printf("(argv_%d) = %s\n", count, argv[count]);
+		}
+	} else {
+		printf("The command had no other arguments (args_none).\n");
+	}
+
+	return(0);
 }
