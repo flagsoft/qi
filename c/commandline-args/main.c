81e13092ed34a95da8e2ae687bd0f7e4cecf9f5a
Michael <michael.mustun@gmail.com>
2014-12-30 17:48:21 +0100

    Update main.c

--- c/commandline-args/main.c
+++ c/commandline-args/main.c
@@ -1,1 +1,23 @@
-/* */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	char *prog_name = argv[0];
+	int num_args = argc - 1;
+	char *first_arg = argv[1];
+	
+	printf("Program name (prog_name): %s\n", prog_name);
+	printf("Number of arguments (num_args): %d\n", num_args);
+	printf("First argument (first_arg): %s\n", first_arg);
+	
+	if (argc > 1) {
+		int count=0;
+		for (count = 1; count < argc; count++) {
+			printf("(argv_%d) = %s\n", count, argv[count]);
+		}
+	} else {
+		printf("The command had no other arguments (args_none).\n");
+	}
+
+	return(0);
+}
