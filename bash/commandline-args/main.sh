bcd4158f63180d13431ffc02b2855973d51505ec
Michael <michael.mustun@gmail.com>
2015-01-03 16:49:10 +0100

    Update main.sh

--- bash/commandline-args/main.sh
+++ bash/commandline-args/main.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-echo "Number of arguments: $#"
-echo "Program name: $0"
-echo "arg 1: $1"
+echo "Program name (qi__prog_name): $0"
+echo "Number of arguments (qi__num_args): $#"
+echo "arg 1 (qi__first_arg): $1"
 echo "arg 2: $2"
 echo "arg 3: $3"
 
