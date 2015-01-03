7057bd3b3681399e2d1edcb54f5506537f07d96a
Michael <michael.mustun@gmail.com>
2015-01-03 16:46:10 +0100

    Update main.pl

--- perl/commandline-args/main.pl
+++ perl/commandline-args/main.pl
@@ -4,11 +4,11 @@ $prog_name = "";
 $num_args = $#ARGV + 1;
 $first_arg = $ARGV[1];
 
-print "Program name (prog_name): $prog_name\n";
-print "Number of arguments (num_args): $num_args\n";
-print "First argument (first_arg): $first_arg\n";
+print "Program name (qi__prog_name): $prog_name\n";
+print "Number of arguments (qi__num_args): $num_args\n";
+print "First argument (qi__first_arg): $first_arg\n";
 
-print "List of arguments (list_args): ";
+print "List of arguments (qi__list_args): ";
 foreach $argnum (0 .. $#ARGV) {
 	print "$ARGV[$argnum]| ";
 }
