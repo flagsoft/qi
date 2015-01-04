a54df3fb21418c5904c91cc1670a4dc6a0dff9c7
Michael <michael.mustun@gmail.com>
2015-01-04 19:48:36 +0100

    Update main.cpp

--- cpp/commandline-args/main.cpp
+++ cpp/commandline-args/main.cpp
@@ -4,22 +4,21 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  char *prog_name = argv[0];
-  int num_args = argc - 1;
-  char *first_arg = argv[1];
+	char *prog_name = argv[0];
+	int num_args = argc - 1;
+	char *first_arg = argv[1];
   
-  cout << "Program name (qi__prog_name): " << prog_name << endl;
-  cout << "Number of arguments (qi__num_args): " << num_args << endl;
-  cout << "First argument (qi__first_arg): " << first_arg << endl;
+	cout << "Program name (qi__prog_name): " << prog_name << endl;
+	cout << "Number of arguments (qi__num_args): " << num_args << endl;
+	cout << "First argument (qi__first_arg): " << first_arg << endl;
 
-  if (argc > 1) {
-    int count=0;
-    for (count = 1; count < argc; count++) {
-      cout << "(qi__argv_" << count << " = " << argv[count] << endl;
-    }
-  } else {
-    cout << "The command had no other arguments (qi__args_none)." << endl;
-  }
-
-  return(0);
+	if (argc > 1) {
+		int count=0;
+		for (count = 1; count < argc; count++) {
+			cout << "(qi__argv_" << count << " = " << argv[count] << endl;
+		}
+	} else {
+		cout << "The command had no other arguments (qi__args_none)." << endl;
+	}
+	return(0);
 }
