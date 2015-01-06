5a46b7874c02a6201c68ac28a16b61aa3de4ef33
Michael Mustun <michi@imac.lan>
2015-01-06 14:31:55 +0100

    .

--- cpp/class/MyClass.h
+++ cpp/class/MyClass.h
@@ -2,10 +2,10 @@
 
 class MyClass {
 
-	MyClass();  // (qi__constructor)
-	~MyClass(); // (qi__destructor)
-
 	public:
+		MyClass();  // (qi__constructor)
+		~MyClass(); // (qi__destructor)
+
    		void setValue(int in_val);
    		int getValue();
 
---
+++ cpp/class/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+#include "MyClass.h"
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+
+	MyClass *myObject = new MyClass();
+
+
+}
