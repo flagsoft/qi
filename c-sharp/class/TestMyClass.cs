9a12d9aceb445e5695a85685ed9bffbd850708ce
Michael <michael.mustun@gmail.com>
2015-01-07 20:32:39 +0100

    Update TestMyClass.cs

--- c-sharp/class/TestMyClass.cs
+++ c-sharp/class/TestMyClass.cs
@@ -1,13 +1,11 @@
 class TestMyClass
 {
 	static void Main()
-    {
-
-    	MyClass *myObject = new MyClass();
-
-
-        // -- Keep the console window open in debug mode
-        Console.WriteLine("Press any key to exit.");
-        Console.ReadKey();
+	{
+		MyClass *myObject = new MyClass();
+		
+		// -- Keep the console window open in debug mode
+		Console.WriteLine("Press any key to exit.");
+		Console.ReadKey();
     }
 }
