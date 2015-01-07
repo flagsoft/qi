5efbfb3ba8cdaeb5c1c730b4f68e227b01da3ad2
Michael <michael.mustun@gmail.com>
2015-01-07 20:21:26 +0100

    init

---
+++ c-sharp/class/TestMyClass.cs
@@ -0,0 +1,13 @@
+class TestMyClass
+{
+	static void Main()
+    {
+
+    	MyClass *myObject = new MyClass();
+
+
+        // -- Keep the console window open in debug mode
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
